deda4624d12750c3ab8837e88a9f9d14b397958e
Luke Parker <lukeparker5132@gmail.com>
2023-11-20 01:34:48 -0500

    Removes lines which clippy treats as errors, not just warnings

--- client/api/src/lib.rs
+++ client/api/src/lib.rs
@@ -69,7 +69,7 @@ pub mod utils {
 				return Ok(false)
 			}
 
-			let current = current.as_ref().map(|(c, p)| (c, p));
+			let current = current.as_ref();
 
 			let mut hash = hash;
 			if let Some((current_hash, current_parent_hash)) = current {
